36e7e669af0934104369cca1074c3594d94d49ed
vkrtecek <krtek@zlin6.cz>
2017-05-14 02:40:38 +0200

    Add files via upload

---
+++ Tabor 2016/scripty/datumChange.php
@@ -0,0 +1,18 @@
+<?php
+$year = $_REQUEST['year'];
+$month = $_REQUEST['month'];
+$day = $_REQUEST['day'];
+$hour = $_REQUEST['hour'];
+$minute = $_REQUEST['minute'];
+
+//echo $year.'-'.$month.'-'.$day.'T'.$hour.':'.$minute;
+
+$str = file_get_contents( '../../index.php' );
+$str = str_replace( "var year= ", 'var year= '.$year.';//', $str );
+$str = str_replace( "var mon = ", 'var mon = '.$month.';//', $str );
+$str = str_replace( "var day = ", 'var day = '.$day.';//', $str );
+$str = str_replace( "var hod = ", 'var hod = '.$hour.';//', $str );
+$str = str_replace( "var mnt = ", 'var mnt = '.$minute.';//', $str );
+file_put_contents('../../index.php', $str );
+echo '<p>Změněno</p>';
+?>
